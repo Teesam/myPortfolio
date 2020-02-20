8a15188708db9f02c71317fe12ad64a019f9f6e7
Olayiwola Samuel <tolayiwola19@yahoo.com>
2020-02-20 01:07:55 +0100

    first change

---
+++ script.js
@@ -0,0 +1,11 @@
+const navigationBtn = document.getElementById('header-button');
+const sectionB = document.querySelector('.section-b');
+const sectionC = document.querySelector('.section-c');
+const footer = document.querySelector('.footer');
+
+
+// navigationBtn.addEventListener('click', ()=>{
+//     footer.style.display = 'block';
+//     sectionC.style.display = 'block';
+//     sectionB.style.display = 'block';
+// })
